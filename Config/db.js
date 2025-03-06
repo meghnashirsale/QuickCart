bcd2901040bffe22c6be7fd10f81528aa50a67ef
Meghna Shirsale <86151439+meghnashirsale@users.noreply.github.com>
2025-03-06 11:59:19 +0530

    First Commit

---
+++ Config/db.js
@@ -0,0 +1,25 @@
+//mongodb+srv://meghnashirsale:<db_password>@cluster0.2w6ax.mongodb.net
+
+import mongoose, { connect } from "mongoose";
+
+let cached=global.mongoose
+ if(!cached){
+    cached=global.mongoose={conn:null,promise:null}
+ }
+
+ async function connectDB() {
+    if(cached.conn){
+        return cached.conn
+    }
+    if(!cached.promise){
+        const opts ={
+            bufferCommands:false
+        }
+        cached.promise=mongoose.connect(`${process.env.MONGODB_URI}/quickcart`,opts).then(mongoose =>{
+            return mongoose
+        })
+    }
+    cached.conn = await cached.promise
+    return cached.conn
+ }
+ export default connectDB
